c316574620cb63d3934a0214d99a1127ef1f209e
kofi-teddy <agudogot@gmail.com>
2021-03-23 13:47:04 +0000

    db updates

--- config/database.js
+++ config/database.js
@@ -1,19 +1,34 @@
+// module.exports = ({ env }) => ({
+//   defaultConnection: 'default',
+//   connections: {
+//     default: {
+//       connector: 'mongoose',
+//       settings: {
+//         host: env('DATABASE_HOST', 'cluster0.bktws.mongodb.net'),
+//         srv: env.bool('DATABASE_SRV', true),
+//         port: env.int('DATABASE_PORT', 27017),
+//         database: env('DATABASE_NAME', 'restaurant-biz'),
+//         username: env('DATABASE_USERNAME', 'admin'),
+//         password: env('DATABASE_PASSWORD', 'admin123'),
+//       },
+//       options: {
+//         authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
+//         ssl: env.bool('DATABASE_SSL', true),
+//       },
+//     },
+//   },
+// });
+
 module.exports = ({ env }) => ({
-  defaultConnection: 'default',
+  defaultConnection: "default",
   connections: {
     default: {
-      connector: 'mongoose',
+      connector: "mongoose",
       settings: {
-        host: env('DATABASE_HOST', 'cluster0.bktws.mongodb.net'),
-        srv: env.bool('DATABASE_SRV', true),
-        port: env.int('DATABASE_PORT', 27017),
-        database: env('DATABASE_NAME', 'restaurant-biz'),
-        username: env('DATABASE_USERNAME', 'admin'),
-        password: env('DATABASE_PASSWORD', 'admin123'),
+        uri: env("DATABASE_URI"),
       },
       options: {
-        authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
-        ssl: env.bool('DATABASE_SSL', true),
+        ssl: true,
       },
     },
   },
